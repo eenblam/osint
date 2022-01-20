35d6381c1debc72861b11e8e320137ed5df44ca8
eenblam <>
2022-01-20 12:12:54 -0800

    document tiktok scroll issue

--- tiktok/bookmarklet.js
+++ tiktok/bookmarklet.js
@@ -1,5 +1,6 @@
 // Alert with creation time of currently viewed channel
 // Shows Unix timestamp as well as (local) datetime.
+// As you scroll to a new video, you'll have to refresh before activating bookmark.
 javascript:
 let d = window['SIGI_STATE'].UserModule.users;
 let user = d[Object.keys(d)[0]];
