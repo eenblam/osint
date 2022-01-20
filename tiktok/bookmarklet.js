b6e6de1645ea03a3b40b7b85f510c65d5bab5060
eenblam <>
2022-01-20 11:58:22 -0800

    Add tiktok creation time bookmarklet

---
+++ tiktok/bookmarklet.js
@@ -0,0 +1,7 @@
+// Alert with creation time of currently viewed channel
+// Shows Unix timestamp as well as (local) datetime.
+javascript:
+let d = window['SIGI_STATE'].UserModule.users;
+let user = d[Object.keys(d)[0]];
+let ts = user.createTime;
+alert(ts+'\n'+new Date(parseInt(ts)));
